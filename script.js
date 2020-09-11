51e5f8bfceb8b2383b49b9ace65c6cd15b64daf3
Louie Camacho <louiecamacho@Elizabeths-MacBook-Air.local>
2020-09-10 22:23:11 -0500

    converted from http request to fetch

--- script.js
+++ script.js
@@ -4,39 +4,32 @@ const btn = document.querySelector("button");
 const counterText = document.querySelector(".counter");
 
 /* breaking bad api */
-let request = new XMLHttpRequest();
-request.open('GET', "https://breaking-bad-quotes.herokuapp.com/v1/quotes/100", true)
-request.onload = function () {
-  // Begin accessing JSON data here
-  let data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-		
-		console.log(data);
-		quote.innerText = data[0].quote;	
-		credit.innerText = data[0].author;
+fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes/100')
+.then(res => res.json())
+.then(data => {
 
-
-getQuote = () => {
-	quote.classList.remove("show");
-	setTimeout((fade) => {
-		quote.classList.add("show");
-	}, 1000);
-	const quoteIndex = Math.floor(Math.random() * data.length);
-	quote.innerText = data[quoteIndex].quote;
-	credit.innerText = data[quoteIndex].author;
-	counterText.innerText = `${quoteIndex+1} of ${data.length}`; /* quote number tracker */
-};
-
-
-getQuote();
-
-btn.addEventListener("click", getQuote);
-
-
-
-} else {
-    console.log("Gah, it's not working!");
-  }
-}
-
-request.send();
+	// console.log(data);
+	quote.innerText = data[0].quote;	
+	credit.innerText = data[0].author;
+	
+	
+	getQuote = () => {
+		quote.classList.remove("show");
+		setTimeout((fade) => {
+			quote.classList.add("show");
+		}, 1000);
+		const quoteIndex = Math.floor(Math.random() * data.length);
+		quote.innerText = data[quoteIndex].quote;
+		credit.innerText = data[quoteIndex].author;
+		counterText.innerText = `${quoteIndex+1} of ${data.length}`; /* quote number tracker */
+	};
+	
+	
+	getQuote();
+	
+	btn.addEventListener("click", getQuote);
+	
+})
+.catch(err => {
+	console.log("Did not work.", err)
+});
